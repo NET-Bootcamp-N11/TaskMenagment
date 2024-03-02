4b11d05945876a309e86de22ee2f38a7be4a9052
Tursunboev0227 <hayotillotursunboev7@gmail.com>
2024-03-02 13:44:06 +0500

    Add project files.

---
+++ TaskMenagment.Domain/Entities/Model/Programmer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMenagment.Domain.Enums;
+
+namespace TaskMenagment.Domain.Entities.Model
+{
+    public class Programmer
+    {
+        public int Id { get; set; }
+        public string FullaName { get; set; }
+        public string About{ get; set; }
+        public Field Field { get; set; }
+    }
+}
---
+++ TaskMenagment.Domain/Enums/Permissions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMenagment.Domain.Enums
+{
+    public enum Permissions
+    {
+        CreateProgrammer = 1,
+        UpdateProgrammer,
+        DeleteProgrammer,
+        GetProgrammerById,
+        CreateTask,
+        UpdateTask,
+        DeleteTask,
+        GetTaskById,
+    }
+}
